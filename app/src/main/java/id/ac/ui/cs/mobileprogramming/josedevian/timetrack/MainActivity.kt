5f6c3326f6c493e90a7ea70141189ca17da0e82c
josedevian <jdevian98@gmail.com>
2020-11-20 00:18:30 +0700

    add app icons

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/josedevian/timetrack/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/josedevian/timetrack/MainActivity.kt
@@ -1,10 +1,12 @@
 package id.ac.ui.cs.mobileprogramming.josedevian.timetrack
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.res.Configuration
-import android.os.Bundle
-import android.os.Handler
-import android.os.SystemClock
+import android.os.*
 import android.view.LayoutInflater
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
@@ -12,9 +14,12 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import id.ac.ui.cs.mobileprogramming.josedevian.timetrack.adapters.ViewPagerAdapter
 import id.ac.ui.cs.mobileprogramming.josedevian.timetrack.fragments.ListFragment
 import id.ac.ui.cs.mobileprogramming.josedevian.timetrack.fragments.StopwatchFragment
+import id.ac.ui.cs.mobileprogramming.josedevian.timetrack.utils.BatteryPercentageChecker
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -31,12 +36,15 @@ class MainActivity : AppCompatActivity() {
     private var milliSeconds: Int = 0
     private lateinit var mHandler: Handler
     private lateinit var mRunnable: Runnable
+    private val CHANNEL_ID = "channel_id_battery_low"
+    private val notificationId = 101
     var duration: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        createNotificationChannel()
         setUpTabs()
 
         mHandler = Handler()
@@ -139,4 +147,48 @@ class MainActivity : AppCompatActivity() {
             adapter.addFragment(ListFragment(), "Logs")
         }
     }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = R.string.notification_title.toString()
+            val notifDesc = R.string.notification_message.toString()
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = notifDesc.toString()
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(R.string.notification_title.toString())
+            .setContentText(R.string.notification_message.toString())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
+    }
+
+//    fun batteryNotifier() {
+//        val batteryLevel = getBatteryPercentage()
+//        if(getBatteryPercentage() <= 10)
+//    }
+//
+//    fun getBatteryPercentage(context: Context): Int {
+//        return if (Build.VERSION.SDK_INT >= 21) {
+//            val bm = context.getSystemService(BATTERY_SERVICE) as BatteryManager
+//            bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+//        } else {
+//            val iFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+//            val batteryStatus: Intent? = context.registerReceiver(null, iFilter)
+//            val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+//            val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+//            val batteryPct = level / scale.toDouble()
+//            (batteryPct * 100).toInt()
+//        }
+//    }
 }
