addc70f3c6c8846cc97ac4502ed28e377ed71449
josedevian <jdevian98@gmail.com>
2021-01-02 19:26:17 +0700

    add opengl flashes, runtime permission

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/josedevian/timetrack/broadcastreceiver/GeofenceBroadcastReceiver.kt
+++
@@ -1,4 +0,0 @@
-package id.ac.ui.cs.mobileprogramming.josedevian.timetrack.broadcastreceiver
-
-class GeofenceBroadcastReceiver {
-}
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/josedevian/timetrack/geofence/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.mobileprogramming.josedevian.timetrack.broadcastreceiver
+
+import android.content.BroadcastReceiver
+
+
+class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+}
